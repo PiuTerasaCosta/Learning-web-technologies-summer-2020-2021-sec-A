dae9f1118d858389406cb6937eaea2801b9cafa5
PiuTerasaCosta <piucosta98@gmail.com>
2021-06-13 10:36:43 +0600

    php intro

---
+++ LAB_TASK_03_PHP_INTRO/3.php
@@ -0,0 +1,13 @@
+<?php   
+
+$a=5;
+    
+        
+        if(($a % 2) == 0){  
+            echo "$a is an Even number";  
+        }
+        else{  
+            echo "$a is Odd number";  
+        }  
+     
+?>  
