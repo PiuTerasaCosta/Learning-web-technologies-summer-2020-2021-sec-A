d634d7794d915e23fb025415852197ad76cc5583
PiuTerasaCosta <piucosta98@gmail.com>
2021-06-13 10:21:30 +0600

    php intro

---
+++ LAB_TASK_03_PHP_INTRO/1.php
@@ -0,0 +1,9 @@
+<?php
+    $length= 10;
+    $width=20;
+    $Rectangle=$length*$width;
+    $p=$length + $width;
+    $perimeter= 2*$p;
+    echo "Rectangle= $Rectangle <br>";
+    echo "perimeter=$perimeter"
+?>
---
+++ LAB_TASK_03_PHP_INTRO/loop.php
@@ -0,0 +1,5 @@
+<?php
+for ($x = 1; $x <= 10; $x++) {
+  echo "PIU TERASA COSTA </br> ";
+}
+?>
